6fede94364abf0141a6b0f30b184f523cf424a3b
MorganAllengit <MorganAllen@byui.edu>
2022-04-19 14:49:56 -0600

    updated hello worlds code

--- print('Hello World!').py
+++ print('Hello World!').py
@@ -1,1 +1,11 @@
-print('\n Hello World! \n')
+print('Hello World!\n')
+
+hw1 = 'Hello World!'
+print(f'{hw1}\n')
+
+hw2 = 'hello world'
+print(f'{hw2.title()}!\n')
+
+hw3 = 'hello'
+hw4 = 'world'
+print(f'{hw3.capitalize()} {hw4.capitalize()}!\n')
